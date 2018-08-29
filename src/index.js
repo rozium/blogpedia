c859070bd49094a8deef7732f2a4726e68980b2a
um <Fahrurrozi31@gmail.com>
2018-08-29 15:22:45 +0700

    merge develop

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import { Provider } from 'react-redux'
 import configureStore from "./redux/configureStore"
 import App from './App';
